b3c7750270b520ece4ec1cafca749f412edc9141
raisatya <satyanlpid1@gmail.com>
2024-05-22 10:38:01 +0530

    Backend start script changes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 const prodOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2]
 const devOrigin = ['http://localhost:5173']
-const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigins : devOrigin
+const allowedOrigins = process.env.NODE_ENV == 'production' ? prodOrigins : devOrigin
 app.use(cors({
     origin: (origin, callback) => {
       if(allowedOrigins.includes(origin)) {
